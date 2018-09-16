e729bc3d60f92917ffb2e8a61080cf6bbf7556a0
hongghe <364930855@qq.com>
2018-09-16 14:05:03 +0800

    The default properties.

---
+++ src/main/java/com/example/hadoopdemo/HadoopDemoProperties.java
@@ -0,0 +1,7 @@
+package com.example.hadoopdemo;
+
+/**
+ * @author hongghe 2018/9/15
+ */
+public class HadoopDemoProperties {
+}
---
+++ src/main/java/com/example/hadoopdemo/service/PackageInfo.java
@@ -0,0 +1,7 @@
+package com.example.hadoopdemo.service;
+
+/**
+ * @author hongghe 2018/9/15
+ */
+public class PackageInfo {
+}
